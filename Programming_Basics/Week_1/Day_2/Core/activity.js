5d37e594a67dee90a462a0c28cbc31ec611d9d04
iheb cherni <eheebhob@gmail.com>
2024-09-24 14:36:38 +0100

    test

---
+++ Programming_Basics/Week_1/Day_2/Core/activity.js
@@ -0,0 +1,21 @@
+var i 
+var year = 0
+for (i=1996;i<=2024;i++){
+
+  year = year +1
+
+  if (year == 11){
+    console.log ("happy decade of js")
+  year = 1   
+  }
+  else if (i==2024){
+    console.log ("HAPPY 28 year")
+
+  }
+else {
+  console.log (i,"JS is Awesome")
+
+}
+  
+}
+
