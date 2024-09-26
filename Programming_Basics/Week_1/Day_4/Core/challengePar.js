d1f8fa8ffea248ccbbebadb545e9c836ab0e296f
iheb cherni <eheebhob@gmail.com>
2024-09-26 13:21:31 +0100

    challengepar

---
+++ Programming_Basics/Week_1/Day_4/Core/challengePar.js
@@ -0,0 +1,40 @@
+function greet(myname){
+  console.log("Good day"+ myname+ "!")
+}
+var myname="Anakin"
+greet(myname)
+
+
+function greet(myname,time){
+  console.log("Good "+time+" "+myname+ "!")
+}
+var time="Day"
+var myname="Anakin"
+greet(myname,time)
+
+
+
+function greet(name, time){
+  if(name=="Count Dooku"){
+    console.log("I'm coming for you, Dooku!")
+  }
+  else(
+    console.log("Good "+time+" "+name+"!")
+  )
+}
+var name="Count Dooku"
+var time="Day"
+greet(name, time)
+
+
+
+
+
+
+
+
+
+
+
+
+
